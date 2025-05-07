2394b336daf48e9331f7ab76d641931db450cce0
Namrata Lamichhane <153053387+Namarata69@users.noreply.github.com>
2025-05-07 10:55:54 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask #flask is module and Flask is class
+from flask import Flask, render_template #flask is module and Flask is class
 app = Flask(__name__) #We create an object of the class
 #Now, we need to create a route for our application
 #route is simply the part of url after the domain name
 @app.route('/') #This is a decorator
-def hello_world():
-  return "Hello Gremlin!"
+def hello_jovian():
+  return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
